db67fea967e008719c5917ba5f10fe7de1db20be
Indiramartinez <indirasilva.022@gmail.com>
2025-02-27 22:41:53 -0600

    In class assignment answers

---
+++ class_notes.R
@@ -0,0 +1,9 @@
+to check your data you use glimpse() will show all the list
+the function head() default shows you the first 6 rows_append()
+there is also a function called tail() to check the end, last 2 rows
+slice() shows you specific rows 
+
+getwd() to get files in your working directory, when you knit this is where your 
+file goes
+you can also set your working directory for reproducibility you create a path 
+accessible for others when you share it
